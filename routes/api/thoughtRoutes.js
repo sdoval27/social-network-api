543995b73d499eb98f15cd1a4b485b7facae752c
unknown <sashardoval@gmail.com>
2023-04-29 17:52:57 -0700

    routes pseudocode

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+//GET to get all thoughts
+
+//GET to get a single thought by its _id
+
+//POST to create a new thought 
+//don't forget to push the created thought's _id to the associated user's thoughts array field
+
+//PUT to update a thought by its _id
+
+//DELETE to remove a thought by its _id
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,9 @@
+//get all users
+
+//get one user by id
+
+//post a new user
+
+//PUT to update a user by its _id
+
+//DELETE to remove user by its _id
